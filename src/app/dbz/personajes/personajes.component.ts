637bebc2ca29b1efcdd4569c1463afc31bbbcc10
Armando Leonardo Garcia Zambrano <legazagz@outlook.com>
2021-05-18 13:13:22 -0500

    Seccion terminada

---
+++ src/app/dbz/personajes/personajes.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+
+import { DbzService } from '../services/dbz.service';
+
+@Component({
+  selector: 'app-personajes',
+  templateUrl: './personajes.component.html',
+})
+export class PersonajesComponent {
+
+  // @Input() personajes: Personaje[] = [];
+  get personajes(){
+    return this.dbzService.personajes;
+  }
+
+  constructor( private dbzService: DbzService){}
+
+
+}
